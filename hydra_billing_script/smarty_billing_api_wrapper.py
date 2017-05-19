f79e0edf6d2ee2a0baaa2007e975f04ced005208
R242 <acmq@ya.ru>
2017-05-19 18:58:01 +0500

    Promo-tariffs in smarty_billing_api_wrapper.py

--- hydra_billing_script/smarty_billing_api_wrapper.py
+++ hydra_billing_script/smarty_billing_api_wrapper.py
@@ -26,18 +26,40 @@ tariffs = {
     3: (5, 6, 7, 8),
 }
 
+promo_tariffs = {
+    5: (1, 2, 3, 4),
+    6: (5, 6, 7, 8),
+}
+
 api = SmartyBillingAPI(HOST, CLIENT_ID, API_KEY, LOG_ENABLED)
 
 
-def get_tariff(tariff):
+def get_tariff(tariff, tariff_map):
     try:
         tariff_id = int(tariff)
     except:
         return None
-    for key in tariffs:
-        if tariff_id in tariffs[key]:
+    for key in tariff_map:
+        if tariff_id in tariff_map[key]:
             return key
     return None
+
+
+def get_tariff_list(raw_tariff_list, tariff_map):
+    tariff_list = []
+    for raw_tariff in raw_tariff_list:
+        tariff = get_tariff(raw_tariff, tariff_map)
+        if tariff is not None and tariff not in tariff_list:
+            tariff_list.append(tariff)
+    return tariff_list
+
+
+def get_inverted_tariff_list(tariff_list, tariff_map):
+    out_list = []
+    for tariff in tariff_map.keys():
+        if tariff not in tariff_list:
+            out_list.append(tariff)
+    return out_list
 
 
 def write_log(message):
@@ -47,6 +69,7 @@ def write_log(message):
                 log_file.write("%s : %s\n" % (datetime.now().isoformat(' '), message))
         except:
             pass
+
 
 def check_user_exists(user_id):
     try:
@@ -64,8 +87,16 @@ def add_account(user_id, account_id):
     api.account_create(ext_id=user_id, abonement=account_id, allow_login_by_abonement=ACCOUNT_ALLOW_LOGIN_BY_ABONEMENT,
                        active=ACCOUNT_ACTIVE, allow_multiple_login=ACCOUNT_ALLOW_MULTIPLE_LOGIN, 
                        allow_login_by_device_uid=ACCOUNT_ALLOW_LOGIN_BY_DEVICE_UID)
+    
+    
+def assign_tariff(tariff, ext_id):
+    api.customer_tariff_assign(tariff, ext_id=ext_id)
 
+    
+def remove_tariff(tariff, ext_id):
+    api.customer_tariff_remove(tariff, ext_id=ext_id)
 
+    
 def create_user_if_not_exists(user_id, account_id):
     error = check_user_exists(user_id)
     if error == 0:
@@ -80,44 +111,52 @@ def create_user_if_not_exists(user_id, account_id):
     except:
         pass
 
-
+    
 def add_user_tariff(args):
     if len(args) < 3:
         return
     user_id = args[0]
     account_id = args[1]
-    tariff_list = args[2].split(',')
-
+    raw_tariff_list = args[2].split(',')
     create_user_if_not_exists(user_id, account_id)
-    
-    for tariff_id in tariff_list:
-        smarty_tariff_id = get_tariff(tariff_id)         
-        if smarty_tariff_id is None:
-            continue
-        api.customer_tariff_assign(smarty_tariff_id, ext_id=user_id)
+
+    promo_tariff_list = get_tariff_list(raw_tariff_list, promo_tariffs)
+    smarty_tariff_list = get_tariff_list(raw_tariff_list, tariffs)
+    inverted_promo_tariff_list = get_inverted_tariff_list(promo_tariff_list, promo_tariffs)
+        
+    for tariff_id in smarty_tariff_list:
+        assign_tariff(tariff_id, user_id)
+    for promo_tariff_id in inverted_promo_tariff_list:
+        assign_tariff(promo_tariff_id, user_id)        
+    for promo_tariff_id in promo_tariff_list:
+        remove_tariff(promo_tariff_id, user_id)
 
 
 def remove_user_tariff(args):
     if len(args) < 2:
         return
     user_id = args[0]
-    account_id = args[1]
-    
+    account_id = args[1]    
     create_user_if_not_exists(user_id, account_id)
     
     if len(args) >= 3:
-        splitted = args[2].split(',')
-        tariff_list = []
-        for tariff_id in splitted:
-            smarty_tariff_id = get_tariff(tariff_id)
-            if smarty_tariff_id is not None:
-                tariff_list.append(smarty_tariff_id)
+        raw_tariff_list = args[2].split(',')
+        promo_tariff_list = get_tariff_list(raw_tariff_list, promo_tariffs)
+        smarty_tariff_list = get_tariff_list(raw_tariff_list, tariffs)
     else:
-        tariff_list = tariffs.keys()
-    
-    for tariff_id in tariff_list:
-        api.customer_tariff_remove(tariff_id, ext_id=user_id)
+        smarty_tariff_list = tariffs.keys()
+        promo_tariff_list = promo_tariffs.keys()
 
+    inverted_promo_tariff_list = get_inverted_tariff_list(promo_tariff_list, promo_tariffs)
+    
+    for tariff_id in smarty_tariff_list:
+        remove_tariff(tariff_id, user_id)
+    for promo_tariff_id in inverted_promo_tariff_list:
+        remove_tariff(promo_tariff_id, user_id)        
+    for promo_tariff in promo_tariff_list:
+        assign_tariff(promo_tariff, user_id)
+
+        
 def add_user_base_tariff(args):
     pass
 
